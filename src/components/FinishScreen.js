ed9e16d77d635d06d121e06b57cdc7e153eb14fb
yazansedih <yazansedih@gmail.com>
2024-11-22 16:45:38 +0200

    Refactoring React Quiz to Context API

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,8 @@
-function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function FinishScreen() {
+  const { points, highscore, maxPossiblePoints, dispatch } = useQuiz();
+
   const persentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,4 +1,8 @@
-function NextButton({ dispatch, answer, index, numQuestions }) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function NextButton() {
+  const { index, answer, numQuestions, dispatch } = useQuiz();
+
   if (answer === null) return null;
 
   if (index < numQuestions - 1)
